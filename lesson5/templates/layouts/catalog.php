10707a7b4158edeb6cb0ef34f919ea05843bd7d7
Kuldyaev Viacheslav <slavalion1@gmail.com>
2022-03-22 13:02:25 +0300

    Add files via upload

---
+++ lesson5/templates/layouts/catalog.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Gallery</title>
+    <link rel="stylesheet" href="css/catalog.css">
+</head>
+<body>
+<?=$menu?>
+<br/>
+<h3>Gallery of Images</h3>
+<br/>
+<div class="imgdesk">
+<?=$content?>
+</div>
+</body>
+</html>
---
+++ lesson5/templates/layouts/oneimg.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Gallery</title>
+    <link rel="stylesheet" href="/css/oneimg.css">
+</head>
+<body>
+<?=$menu?>
+<br/>
+<div class="imgdesk">
+    <div class="imgimg">
+        <h3>Image</h3>
+        <br/>
+        <?=$content?>
+    </div>
+    <div class="imginfo">
+        <br/>
+        <a href="/catalog">Назад в галерею</a>
+    </div>
+</div>
+</body>
+</html>

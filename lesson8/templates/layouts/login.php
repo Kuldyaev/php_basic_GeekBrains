75d024022d16f0e3a2c8862c4878587770d8d6ad
Kuldyaev Viacheslav <slavalion1@gmail.com>
2022-04-08 01:08:38 +0300

    Add files via upload

---
+++ lesson8/templates/layouts/login.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Gallery</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/login.css">  
+</head>
+<body>
+<main>
+    <?=$header?>    
+    <?=$menu?>
+    <div class="LogPage">
+        <h3>LogPage</h3>
+    </div>
+    <div class="cardDesk">
+        <?=$content?>
+    </div>
+</main>
+<?=$footer?>
+</body>
+</html>
---
+++ lesson8/templates/layouts/orders.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Gallery</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/orders.css">  
+</head>
+<body>
+<main>    
+    <?=$header?>    
+    <?=$menu?>
+    <br/>
+    <div class="backLink">
+        <a href="/cart">Back to cart</a>
+    </div>
+    <div class="cardDesk">
+        <?=$content?>
+    </div>
+</main>
+<?=$footer?>
+</body>
+</html>
